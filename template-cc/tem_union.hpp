9f54902a435d593c1417e69fabb17c82ac83dcee
pinK <87963551+PinkDoc@users.noreply.github.com>
2022-05-26 20:41:35 +0800

    imple a simple union

--- template-cc/tem_union.hpp
+++ template-cc/tem_union.hpp
@@ -1,25 +1,41 @@
 #pragma once
 
- 
+namespace detail {
 
-template <std::size_t S, typename... Args>
-struct union_size 
-{
+template <std::size_t S, typename... Args> struct union_size {
 
-    constexpr static auto value = S;
+  constexpr static auto value = S;
+
 };
-
 
 template <std::size_t T, typename N, typename... Args>
-struct union_size<T,N, Args...>
-{
+struct union_size<T, N, Args...> {
 
-    constexpr static auto value = T > sizeof(N) ? union_size<T, Args...>::value
-                                                : union_size<sizeof(N), Args...>::value;
+  constexpr static auto value = T > sizeof(N)
+                                    ? union_size<T, Args...>::value
+                                    : union_size<sizeof(N), Args...>::value;
+
 };
 
-template <typename... Args>
-struct size
-{
-    constexpr static auto value = union_size<0, Args...>::value;
+template <typename... Args> struct size {
+  constexpr static auto value = union_size<0, Args...>::value;
 };
+
+} // namespace detail
+
+namespace tem
+{
+    using detail::size;
+
+    template <typename... Args>
+    class tem_union
+    {   
+    public:
+      template <typename T> T &as() {
+        return *(T *)(holder);
+      }
+
+    private:
+        char holder[size<Args...>::value];
+    };
+}
