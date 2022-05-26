1108aeea38173dce59836b177d3a2be59bd199bb
pinK <87963551+PinkDoc@users.noreply.github.com>
2022-05-26 20:19:18 +0800

    add union.hpp

---
+++ template-cc/union.hpp
@@ -0,0 +1,22 @@
+#pragma once
+template <std::size_t S, typename... Args>
+struct union_size 
+{
+
+    constexpr static auto value = S;
+};
+
+
+template <std::size_t T, typename N, typename... Args>
+struct union_size<T,N, Args...>
+{
+
+    constexpr static auto value = T > sizeof(N) ? union_size<T, Args...>::value
+                                                : union_size<sizeof(N), Args...>::value;
+};
+
+template <typename... Args>
+struct size
+{
+    constexpr static auto value = union_size<0, Args...>::value;
+};
